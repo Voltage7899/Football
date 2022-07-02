5f4817012112ca995920ea0bca99278dd383908c
Kirill Iva <ivanov.kiace159@yandex.ru>
2022-07-03 00:34:47 +0300

    first

---
+++ app/src/main/java/com/company/footballinfo/FullInfo.kt
@@ -0,0 +1,31 @@
+package com.company.footballinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.company.footballinfo.databinding.ActivityFullInfoBinding
+import com.squareup.picasso.Picasso
+
+class FullInfo : AppCompatActivity() {
+    lateinit var binding: ActivityFullInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityFullInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val item= intent.getSerializableExtra("match") as mMatch
+        Picasso.get().load(item.firstImage).fit().into(binding.fullFirstImage)
+        Picasso.get().load(item.secondImage).fit().into(binding.fulladdSecondImage)
+
+        binding.fullnameFirstTema.setText(item.firstTeam)
+        binding.fullnameSecTeam.setText(item.secondTeam)
+
+        binding.fulllinkFirstTeam.setText(item.firstImage)
+        binding.fulllinkSecTeam.setText(item.secondImage)
+
+        binding.fullDate.setText(item.data)
+
+        binding.fullscore.setText(item.score)
+
+        binding.fullDesc.setText(item.desc)
+    }
+}
---
+++ app/src/main/java/com/company/footballinfo/mMatch.kt
@@ -0,0 +1,6 @@
+package com.company.footballinfo
+
+import java.io.Serializable
+
+class mMatch(val id:Int?=0,val firstTeam:String?="",val secondTeam:String?="",val score:String?="",val data:String?="",val firstImage:String?="",val secondImage:String?="",val desc:String?="") :Serializable {
+}
